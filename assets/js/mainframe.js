929465b87313b2e64ab423079d6c18b7fbd19e54
saswati <saswati@theqa.in>
2024-05-08 12:53:04 +0530

    Responsive

--- assets/js/mainframe.js
+++ assets/js/mainframe.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // documentReady
     // ===========   slider-students-feeling slider
     var swiper1 = new Swiper(".banner-section-home .swiper-container", {
         slidesPerView: 1,
@@ -50,86 +51,57 @@ $(document).ready(function () {
         }, 500);
     });
 
-
-
-
-   // phone call 
-    var myID = document.getElementById("phone-call");
-
-    var myScrollFunc = function() {
-        var y = window.scrollY;
-        var windowHeight = window.innerHeight;
-        var documentHeight = document.body.clientHeight;
-        // Calculate the bottom position
-        var bottomPosition = documentHeight - windowHeight - y;
-        if (bottomPosition < 5) {
-          myID.className = "phone-call-wrap show";
-        } 
-        else {
-          myID.className = "phone-call-wrap";
-        }
-    };
-
-  window.addEventListener("scroll", myScrollFunc);
-  // phone call  end
-
-  // pagination-container
-    var items = $(".event-list .event-item");
-    var numItems = items.length;
-    var perPage = 6;
-    items.slice(perPage).hide();
-    if ($('.event-list').length > 0) {
-      $('#pagination-container').pagination({
-          items: numItems,
-          itemsOnPage: perPage,
-          prevText: "<i class='fa-solid fa-chevron-left'></i>",
-          nextText: "<i class='fa-solid fa-chevron-right'></i>",
-          onPageClick: function (pageNumber) {
-              var showFrom = perPage * (pageNumber - 1);
-              var showTo = showFrom + perPage;
-              items.hide().slice(showFrom, showTo).show();
-          }
-      });
-    }
-  // pagination-container end
   // select2
-  if ($('.select2').length > 0) {
+    if ($('.select2').length > 0) {
       $(".select2").select2();
       
-  }
-  
+    }
 
-  // enquiry-radio-list
-  if ($('.enquiry-form').length > 0) {
-    $('.enquiry-radio-list input[type="radio"]').click(
-        function () {
-            const inputValue =  $(this).attr("value");
-            const targetBox =  $("." + inputValue);
-            $(".radio-open-box").not(targetBox).hide();
-            $(targetBox).show();
-        }
-    );
-    $('.type-list input[type="radio"]').click(
-        function () {
-            const inputValue =  $(this).attr("value");
-            const targetBox =  $("." + inputValue);
-            $(".type-text-box").not(targetBox).hide();
-            $(targetBox).show();
-        }
-    );
-  }
-
+    // enquiry-radio-list
+    if ($('.enquiry-form').length > 0) {
+      $('.enquiry-radio-list input[type="radio"]').click(
+          function () {
+              const inputValue =  $(this).attr("value");
+              const targetBox =  $("." + inputValue);
+              $(".radio-open-box").not(targetBox).hide();
+              $(targetBox).show();
+          }
+      );
+      $('.type-list input[type="radio"]').click(
+          function () {
+              const inputValue =  $(this).attr("value");
+              const targetBox =  $("." + inputValue);
+              $(".type-text-box").not(targetBox).hide();
+              $(targetBox).show();
+          }
+      );
+    }
 
 
 
 
+      // pagination-container
+      var items = $(".event-list .event-item");
+      var numItems = items.length;
+      var perPage = 6;
+      items.slice(perPage).hide();
+      if ($('.event-list').length > 0) {
+        $('#pagination-container').pagination({
+            items: numItems,
+            itemsOnPage: perPage,
+            prevText: "<i class='fa-solid fa-chevron-left'></i>",
+            nextText: "<i class='fa-solid fa-chevron-right'></i>",
+            onPageClick: function (pageNumber) {
+                var showFrom = perPage * (pageNumber - 1);
+                var showTo = showFrom + perPage;
+                items.hide().slice(showFrom, showTo).show();
+            }
+        });
+      }
+      
+    // pagination-container end
+   
 
-
-
-});
-
-
-  $(document).ready(function(event){
     $('.applicaltion-select select').on('change', function(){
       const $this = $(this);
       $('.product-item').hide();
@@ -146,31 +118,33 @@ $(document).ready(function () {
         $('.'+$this.val()).show();
     });
 
+    
+
    $('#recentSelect').on('change', function(){
         const  $this = $(this);
         $('.event-item').hide();
         $('.'+$this.val()).show();
     });
 
+  // documentReady end
   });
 
+   // phone call 
+  const phoneCall = document.getElementById("phone-call");
+  const phoneScrollFunc = function() {
+      var y = window.scrollY;
+      var windowHeight = window.innerHeight;
+      var documentHeight = document.body.clientHeight;
+      // Calculate the bottom position
+      var bottomPosition = documentHeight - windowHeight - y;
+      if (bottomPosition < 5) {
+        phoneCall.className = "phone-call-wrap show";
+      } 
+      else {
+        phoneCall.className = "phone-call-wrap";
+      }
+  };
 
+ window.addEventListener("scroll", phoneScrollFunc);
+ // phone call  end
 
-// if ($('.testimonial-section').length > 0) {
-//   $('[data-fancybox]').fancybox({
-//     // Options will go here
-//     buttons : [
-//       'close'
-//     ],
-//     wheel : false,
-//     transitionEffect: "slide",
-//     // thumbs          : false,
-//     // hash            : false,
-//     loop            : true,
-//     // keyboard        : true,
-//     toolbar         : false,
-//     // animationEffect : false,
-//     // arrows          : true,
-//     clickContent    : false
-//   });
-// }
